101f1fe68cb35aa609f9601cebbfb079813b92ce
Sebastian Paulsen <paulsen@froglogic.com>
2020-12-01 12:21:25 +0100

    Advanced main

--- 19/main.py
+++ 19/main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-import sys
-import time
-import math
+import sys, time, math
 
 def readInput():
-    file = open("input", "r")
+    if( len( sys.argv ) == 2 ):
+        file = open( sys.argv[1] , "r")
+    else:
+        file = open("input", "r")
     lines = file.readlines()
     return lines
 
