c7260e58f92cf0eea8fd2eed48069f2fc30618e1
Sebastian Paulsen <sebastian_paulsen@paulsenkom.de>
2020-12-01 10:23:25 +0100

    Initial commit

---
+++ 20/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+import time
+import math
+
+def readInput():
+    file = open("input", "r")
+    lines = file.readlines()
+    return lines
+
+def main():
+    lines = readInput()
+    print(lines)
+
+
+if __name__ == "__main__":
+    main()
