4c5be7e4650ee3b2b28a225113bfa1dbed008084
Sebastian Paulsen <sebastian_paulsen@paulsenkom.de>
2020-12-04 10:56:13 +0100

    First steps day 4

--- 4/main.py
+++ 4/main.py
@@ -9,8 +9,33 @@ def readInput():
     lines = file.readlines()
     return lines
 
+def generateEmptyID():
+    return { ["byr", "0"], ["iyr", "0"], ["eyr", "0"], ["hgt", "0"], ["hc," "0"], ["ecl", "0"], ["pid", "0"], ["cid", "0"] }
+
 def partOne( lines ):
-    pass
+    idList = []
+    generatedID = generateEmptyID()
+    for line in lines:
+        if(line == "\n"):
+            idList.append( currentid )
+            generatedID = generateEmptyID()
+            continue
+        line.split( ' ' )
+        line.remove( "" )
+        for element in line:
+            element.split( ':' )
+            generatedID[ element[0] ] = element[1]()
+    counter = 0
+    for currentID in idList:
+        valid = false
+        for element in currentID:
+            if( element ):
+                pass
+        if( valid ):
+            counter += 1
+
+
+
 
 def partTwo( lines ):
     pass
