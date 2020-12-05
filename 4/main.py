e9696e444c9c5a1713228e460980f3ee1dc5da6e
Sebastian Paulsen <sebastian_paulsen@paulsenkom.de>
2020-12-05 18:17:00 +0100

    Solved Day 4

--- 4/main.py
+++ 4/main.py
@@ -10,41 +10,101 @@ def readInput():
     return lines
 
 def generateEmptyID():
-    return { ["byr", "0"], ["iyr", "0"], ["eyr", "0"], ["hgt", "0"], ["hc," "0"], ["ecl", "0"], ["pid", "0"], ["cid", "0"] }
+    return { "byr" :"???", "iyr": "???", "eyr" :"???", "hgt": "???", "hcl": "???", "ecl": "???", "pid": "???", "cid": "???" }
+
+
 
 def partOne( lines ):
     idList = []
     generatedID = generateEmptyID()
     for line in lines:
         if(line == "\n"):
-            idList.append( currentid )
+            idList.append( generatedID )
             generatedID = generateEmptyID()
             continue
-        line.split( ' ' )
-        line.remove( "" )
+        line = line.split( ' ' )
         for element in line:
-            element.split( ':' )
-            generatedID[ element[0] ] = element[1]()
+            element = element.split( ':' )
+            generatedID[ element[0] ] = element[1].replace('\n', '')
+    idList.append( generatedID )
     counter = 0
     for currentID in idList:
-        valid = false
+        valid = True
         for element in currentID:
-            if( element ):
-                pass
+            if( element != "cid" ):
+                if currentID[ element ] == "???":
+                    valid = False
         if( valid ):
             counter += 1
+    print( "PartOne: ", counter )
+    return( idList )
 
+def partTwo( idList ):
+    counter = 0
+    Checklist = [ "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" ]
+    print(len( idList ))
+    for currentID in idList:
+        invalid = False
+        for check in Checklist:
+            element = currentID[check]
+            try:
+                if( check == "byr" ):
+                    if( int(element) > 2002 or int(element) < 1920):
+                        invalid = True
+                        #print("Invalid:", element, check)
+                elif( check == "iyr" ):
+                    if( int(element) < 2010 or int(element) > 2020):
+                        invalid = True
+                        #print("Invalid:", element, check)
+                elif( check == "eyr"):
+                    if( int(element) < 2020 or int(element) > 2030):
+                        invalid = True
+                        #print("Invalid:", element, check)
+                elif( check == "hgt"):
+                    if( "cm" in element ):
+                        element = element.replace("cm","")
+                        if ( int(element) > 193 or int(element) < 150 ):
+                            invalid = True
+                            #print("Invalid:", element, check)
+                    elif( "in" in element ):
+                        #print( check, element )
+                        element = element.replace("in","")
+                        if ( int(element) > 76 or int(element) < 59 ):
+                            invalid = True
+                            #print("Invalid:", element, check)
+                    else:
+                        invalid = True
+                elif( check == "hcl"):
+                    if( element[0] != "#" ):
+                            invalid = True
+                    element = element.replace('#','' )
+                    number = int( element, 16 )
+                    if( number < 0 or number > 0xFFFFFF ):
+                        invalid = True
+                        #print("Invalid:", element, check)
+                elif( check == "ecl"):
+                    if( element not in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]):
+                        invalid = True
+                        #print("Invalid:", element, check)
+                elif( check == "pid"):
+                    if( len(element) != 9 or int( element ) < 0 or int(element) > 999999999 ):
+                        invalid = True
+                else:
+                    invalid = True
+                    print("Invalid:", element, check)
+            except Exception as e:
+                #print( e, check, element )
+                invalid = True
+        if( not invalid ):
+            print( currentID )
+            counter += 1
+    print( "PartTwo: ", counter )
 
-
-
-def partTwo( lines ):
-    pass
 
 def main():
     lines = readInput()
-    print(lines)
-    partOne( lines )
-    partTwo( lines )
+    idList = partOne( lines )
+    partTwo( idList )
 
 
 if __name__ == "__main__":
