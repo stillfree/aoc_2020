a72eec668cb891e58343fda10b5bacf9053e694f
Sebastian Paulsen <sebastian_paulsen@paulsenkom.de>
2020-12-05 18:44:18 +0100

    Day 5

--- 5/main.py
+++ 5/main.py
@@ -9,15 +9,51 @@ def readInput():
     lines = file.readlines()
     return lines
 
+def getRow( string ):
+    string = string.replace('B','1')
+    string = string.replace('F','0')
+    return int( string, 2 )
+
+def getColumn( string ):
+    string = string.replace('R','1')
+    string = string.replace('L','0')
+    return int( string, 2 )
+
 def partOne( lines ):
+    seatIDs = []
+    for line in lines:
+        line =line.replace("\n","")
+        rowStr = line[:7]
+        columnStr = line[7:]
+        row = getRow( rowStr )
+        column = getColumn( columnStr )
+        seatID = row * 8 + column
+        seatIDs.append( seatID )
+    print("PartOne: ", max( seatIDs ) )
     pass
 
 def partTwo( lines ):
-    pass
+    array = []
+    for i in range( 128 ):
+        liste = []
+        for j in range( 8 ):
+            liste.append( 0 )
+        array.append( liste )
+    for line in lines:
+        line =line.replace("\n","")
+        rowStr = line[:7]
+        columnStr = line[7:]
+        row = getRow( rowStr )
+        column = getColumn( columnStr )
+        array[row][column] = 1
+
+    for i in range( 12, 110 ):
+        for j in range( 7 ):
+            if( array[i][j] == 0 ):
+                print("PartTwo: ", i*8+ j )
 
 def main():
     lines = readInput()
-    print(lines)
     partOne( lines )
     partTwo( lines )
 
