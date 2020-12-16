85a1e20b92b4a5e2c3a9bca07c5ba876ce4d2c34
Sebastian Paulsen <sebastian_paulsen@paulsenkom.de>
2020-12-16 09:38:48 +0100

    day 16 first

--- 16/main.py
+++ 16/main.py
@@ -10,14 +10,39 @@ def readInput():
     return lines
 
 def partOne( lines ):
-    pass
+    print(lines)
+    i = 0
+    availableNumbers = {}
+    indexX = lines.index( "your ticket:\n")
+    indexY = lines.index( "nearby tickets:\n")
+    restrictions = lines[:indexX-1]
+    myticket = lines[indexX+1: indexX+2][0].strip()
+    otherTickets = lines[indexY+1:]
+    print( restrictions, myticket )
+    for res in restrictions:
+        res = res.strip().split( " " )
+        print( res )
+        for e in res:
+            if( "-" in e ):
+                e = e.split("-")
+                for i in range( int(e[0]), int(e[1]) +1 ):
+                    availableNumbers[str(i)] = 1
+    counter = 0
+    for other in otherTickets:
+        elements = other.strip().split(",")
+        for ele in elements:
+            if( ele in availableNumbers.keys() ):
+                pass
+            else:
+                print( "NOT", ele )
+                counter += int(ele)
+    print( counter )
 
 def partTwo( lines ):
     pass
 
 def main():
     lines = readInput()
-    print(lines)
     partOne( lines )
     partTwo( lines )
 
